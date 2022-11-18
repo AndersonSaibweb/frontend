ef8d8fcb731bf815c23308887da836ba19cc0289
AndersonSaibweb <anderson.net1@gmail.com>
2022-11-18 07:23:18 -0300

    feat: new version with reflesh

--- public/src/const.js
+++ public/src/const.js
@@ -1,5 +1,4 @@
 _BASE_URL = 'http://webservice.grupomariza.com.br:8085/'
-// _BASE_URL = 'http://localhost:8087/'
 _EP_LOGIN = 'api/v1/auth/login'
 _EP_PROFILE = 'api/v1/auth/profile'
 _EP_ESTOQUE = 'api/v1/estoque/getEstoqueProdutos'
